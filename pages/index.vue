3f92e0b9550a6c695fd4807b03681d6cfb2bdf15
lavrenderg <m.lavrender@gmail.com>
2021-02-27 19:03:51 +0100

    Update site url and Add Reservations dev

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <h1>Rent a car in a few minutes =></h1>
+  <div class="quick_reservation">
+    <h1>Quick reservation</h1>
+    <p>Location</p>
+    <textarea v-model="location_pickup" placeholder="Pickup"></textarea>
+    <textarea v-model="location_return" placeholder="Return"></textarea>
+    <p>Date</p>
+    <textarea v-model="date_pickup" placeholder="Pickup"></textarea>
+    <textarea v-model="date_return" placeholder="Return"></textarea>
   </div>
 </template>
 
 <script>
 export default {
-  head() {
+  /*head() {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     }
-  },
+  },*/
+  css: ['@~/assets/css/_custom.pcss'],
 }
 </script>
