1d0d136978530e7df51652c31f0dd5476fe64498
lavrenderg <m.lavrender@gmail.com>
2021-03-04 19:17:32 +0100

    Add netlify identity

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,11 @@
 
 <script>
 export default {
+  head() {
+    return {
+      script: { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+    }
+  },
   data() {
     return {
       options: {
