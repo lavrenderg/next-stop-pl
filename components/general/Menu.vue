074f0da687edca2f343f2f1d0c8b0e1f7d3d3d6e
lavrenderg <m.lavrender@gmail.com>
2021-06-13 12:10:47 +0200

    Fix check

--- components/general/Menu.vue
+++ components/general/Menu.vue
@@ -3,20 +3,20 @@
     <v-btn class="back_btn" nuxt x-small @click="closeMenu">Zamknij</v-btn>
     <div class="menu_buttons">
       <div class="btn_with_text1">
-        <v-btn class="menu_btn1" nuxt small plain outlined href="/#locations_div">Nasze oddziały </v-btn>
+        <v-btn class="menu_btn1" nuxt small plain outlined><NuxtLink to="/#locations_div">Nasze oddziały </v-btn>
         <img class="btn_img1" src="@/static/icons/map.png" />
       </div>
       <div class="btn_with_text2">
-        <v-btn class="menu_btn2" nuxt small plain outlined to="/carPage">Oferta aut</NuxtLink></v-btn>
+        <v-btn class="menu_btn2" nuxt small plain outlined><NuxtLink to="/carPage">Oferta aut</NuxtLink></v-btn>
         <img class="btn_img2" src="@/static/icons/deal.png" />
       </div>
       <div class="btn_with_text3">
-        <v-btn class="menu_btn3" nuxt small plain outlined href="/#locations_div">Kontakt</NuxtLink></v-btn>
+        <v-btn class="menu_btn3" nuxt small plain outlined><NuxtLink to="/#locations_div">Kontakt</NuxtLink></v-btn>
         <img class="btn_img3" src="@/static/icons/contact.png" />
       </div>
     </div>
     <div class="test">
-      <!--<div v-if="adminIsLoggedIn">
+      <div v-if="adminIsLoggedIn">
         <v-btn text to="/reservations">Rezerwacje</v-btn>
         <v-btn @click="logout" text>Logout</v-btn>
       </div>
@@ -27,7 +27,7 @@
       <div v-if="this.$fire.auth.currentUser == null">
         <v-btn text to="/register">Rejestracja</v-btn>
         <v-btn text to="/login">Logowanie</v-btn>
-      </div> -->
+      </div>
     </div>
   </v-card>
 </template>
